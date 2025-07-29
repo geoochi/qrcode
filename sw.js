4a5a843c6bf6f787572fbedc61c8e4ad2a34e192
Geoo Chi <geoochi@geoochi.com>
2025-07-29 11:50:30 +0800

    update

--- sw.js
+++ sw.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = 'ultraqr.codes'
+const APP_NAME = 'ultraqr.codes'
+const VERSION = '1.0'
+const CACHENAME = `${APP_NAME}-${VERSION}`
+
 self.addEventListener('install', () => {
   self.skipWaiting()
 })
+
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => Promise.all([self.clients.claim(), ...cacheNames.filter(e => e.startsWith(a) && e !== CACHE_NAME).map(e => caches.delete(e))]))
+    caches.keys().then(cacheNames =>
+      Promise.all([
+        self.clients.claim(),
+        ...cacheNames
+          .filter(cacheName => cacheName.startsWith(APP_NAME) && cacheName !== CACHENAME)
+          .map(cacheName => {
+            return caches.delete(cacheName)
+          }),
+      ])
+    )
   )
 })
+
 self.addEventListener('fetch', event => {
   if (event.request.url.includes('/api/')) return
-  const s = new URL(event.request.url)
-  ;(!/(?:\.css|\.js|\.svg)$/i.test(s.pathname) && s.origin !== self.location.origin) ||
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then(e => e || caches.open(CACHE_NAME).then(h => fetch(event.request).then(n => (n.status !== 200 || h.put(event.request, n.clone()), n))))
+
+  const url = new URL(event.request.url)
+
+  if (!/(?:\.css|\.js|\.svg)$/i.test(url.pathname) && url.origin !== self.location.origin) return
+
+  event.respondWith(
+    caches.match(event.request).then(
+      match =>
+        match ||
+        caches.open(CACHENAME).then(cache =>
+          fetch(event.request).then(response => {
+            if (response.status !== 200) return response
+
+            cache.put(event.request, response.clone())
+            return response
+          })
+        )
     )
+  )
 })
